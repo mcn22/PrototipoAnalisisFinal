c8058a088cd71f546d2f7dc031865fdfdfb08154
Mario Sanchez <mcn_09@live.com>
2021-04-14 13:23:57 -0600

    Logo mas grande

--- assets/js/navbarOut.js
+++ assets/js/navbarOut.js
@@ -98,63 +98,93 @@ var htmlmenuDesktop = `
 document.getElementById('mainNav').innerHTML = htmlmenuDesktop;
 });
 
-$(document).ready(function () {
-var htmlmenuMobile = `
-<a href="index.html"><img class="mobile-logo" src="assets/img/gym-logo.png" alt="Gym"></a>
-<ul class="nav navbar-nav navbar-right menu">
-    <li class="nav-item active">
-        <a class="nav-link" href="index.html">Home</a>
-    </li>
-    <li class="dropdown">
-        <a>Acceso</a>
-        <ul class="sub_menu">
-            <li><a href="Acceso/login.html">Login</a></li>
-            <li><a href="Acceso/register.html">Registro</a></li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a>Cliente</a>
-        <ul class="sub_menu">
-            <li><a href="Cliente/HUM2-001-1.html">Actualizar datos</a></li>
-            <li><a href="Cliente/HUM2-002-1.html">Desactivar cuenta</a></li>
-            <li><a href="Cliente/HUM2-002-2.html">Reactivar cuenta</a></li>
-            <li><a href="Cliente/HUM2-003-1.html">Crear reserva</a></li>
-            <li><a href="Cliente/HUM2-003-2.html">Modificar reserva</a></li>
-            <li><a href="Cliente/HUM2-003-3.html">Lista de reservas</a></li>
-            <li><a href="Cliente/HUM2-003-4.html">Elimina reserva</a></li>
-            <li><a href="Cliente/HUM2-004-1.html">Enviar retroalimentacion</a></li>
-            <li><a href="Cliente/HUM2-004-2.html">Lista de comentarios</a></li>
-            <li><a href="Cliente/HUM2-005-1.html">Ver plan de dieta</a></li>
-            <li><a href="Cliente/HUM2-006-1.html">Ver plan de entrenamiento</a></li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a>Administrador</a>
-        <ul class="sub_menu">
-            <li><a href="Administrador/HUM3-001.html">Administrar usuarios</a></li>
-            <li><a href="Administrador/HUM3-002.html">Administrar muro</a></li>
-            <li><a href="Administrador/HUM3-003.html">Informar usuarios</a></li>
-            <li><a href="Administrador/HUM3-004.html">Administrar reportes</a></li>
-            <li><a href="Administrador/HUM3-005-1.html">Crear cuenta usuario</a></li>
-            <li><a href="Administrador/HUM3-005-2.html">Editar cuentas</a></li>
-        </ul>
-    </li>
-    <li class="dropdown">
-        <a>Pagos</a>
-        <ul class="sub_menu">
-            <li><a href="Pagos/HUMP-0011.html">Asignar membresía</a></li>
-            <li><a href="Pagos/HUMP-0012.html">Ingresar cliente a facturación</a></li>
-            <li><a href="Pagos/HUMP-0013.html">Crear pago</a></li>
-            <li><a href="Pagos/HUMP-0014.html">Aplicar pago</a></li>
-            <li><a href="Pagos/HUMP-0015.html">Crear nota de crédito</a></li>
-            <li><a href="Pagos/HUMP-0015.html">Crear nota de débito</a></li>
-            <li><a href="Pagos/HUMP-0031.html">Balance inicial</a></li>
-            <li><a href="Pagos/HUMP-0032.html">Arqueo de caja</a></li>
-            <li><a href="Pagos/HUMP-0041.html">Establecer balance</a></li>
-            <li><a href="Pagos/HUMP-0042.html">Modificar balance</a></li>
-        </ul>
-    </li>
-</ul>
-`
-document.getElementById('navegacionMobile').innerHTML = htmlmenuMobile;
-});
+$(document).ready(function() {
+    var htmlmenuMobile = `
+    <a href="index.html"><img class="mobile-logo" src="assets/img/gym-logo.png" alt="Gym"></a>
+    <ul class="nav navbar-nav navbar-right menu">
+        <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="dropdown">
+            <a>Acceso</a>
+            <ul class="sub_menu">
+                <li><a href="Acceso/login.html">Login</a></li>
+                <li><a href="Acceso/register.html">Registro</a></li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a>Cliente</a>
+            <ul class="sub_menu">
+                <li><a href="Cliente/HUM2-001-1.html">Actualizar datos</a></li>
+                <li><a href="Cliente/HUM2-002-1.html">Desactivar cuenta</a></li>
+                <li><a href="Cliente/HUM2-002-2.html">Reactivar cuenta</a></li>
+                <li><a href="Cliente/HUM2-003-1.html">Crear reserva</a></li>
+                <li><a href="Cliente/HUM2-003-2.html">Modificar reserva</a></li>
+                <li><a href="Cliente/HUM2-003-3.html">Lista de reservas</a></li>
+                <li><a href="Cliente/HUM2-003-4.html">Elimina reserva</a></li>
+                <li><a href="Cliente/HUM2-004-1.html">Enviar retroalimentacion</a></li>
+                <li><a href="Cliente/HUM2-004-2.html">Lista de comentarios</a></li>
+                <li><a href="Cliente/HUM2-005-1.html">Ver plan de dieta</a></li>
+                <li><a href="Cliente/HUM2-006-1.html">Ver plan de entrenamiento</a></li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a>Administrador</a>
+            <ul class="sub_menu">
+                <li><a href="Administrador/HUM3-001.html">Administrar usuarios</a></li>
+                <li><a href="Administrador/HUM3-002.html">Administrar muro</a></li>
+                <li><a href="Administrador/HUM3-003.html">Informar usuarios</a></li>
+                <li><a href="Administrador/HUM3-004.html">Administrar reportes</a></li>
+                <li><a href="Administrador/HUM3-005-1.html">Crear cuenta usuario</a></li>
+                <li><a href="Administrador/HUM3-005-2.html">Editar cuentas</a></li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a>Pagos</a>
+            <ul class="sub_menu">
+                <li><a href="Pagos/HUMP-0011.html">Asignar membresía</a></li>
+                <li><a href="Pagos/HUMP-0012.html">Ingresar cliente a facturación</a></li>
+                <li><a href="Pagos/HUMP-0013.html">Crear pago</a></li>
+                <li><a href="Pagos/HUMP-0014.html">Aplicar pago</a></li>
+                <li><a href="Pagos/HUMP-0015.html">Crear nota de crédito</a></li>
+                <li><a href="Pagos/HUMP-0015.html">Crear nota de débito</a></li>
+                <li><a href="Pagos/HUMP-0031.html">Balance inicial</a></li>
+                <li><a href="Pagos/HUMP-0032.html">Arqueo de caja</a></li>
+                <li><a href="Pagos/HUMP-0041.html">Establecer balance</a></li>
+                <li><a href="Pagos/HUMP-0042.html">Modificar balance</a></li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a>Empleado</a>
+            <ul class="sub_menu">
+                <li><a href="Empleado/HUEM-001.html">Modificar empleado</a></li>
+                <li><a href="Empleado/HUEM-002.html">Crear dieta</a></li>
+                <li><a href="Empleado/HUEM-002-01.html">Modificar dieta</a></li>
+                <li><a href="Empleado/HUEM-003.html">Crear rutina</a></li>
+                <li><a href="Empleado/HUEM-003-1.html">Modificar rutina</a></li>
+                <li><a href="Empleado/HUEM-004.html">Ver calendario</a></li>
+                <li><a href="Empleado/HUEM-005.html">Crear Métricas</a></li>
+                <li><a href="Empleado/HUEM-005-1.html">Modificar Métricas</a></li>
+                <li><a href="Empleado/HUEM-006.html">Historial cliente</a></li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a>Reporte</a>
+            <ul class="sub_menu">
+                <li><a href="Reporte/HURM-001.html">Clientes activos</a></li>
+                <li><a href="Reporte/HURM-002.html">Arqueo de caja</a></li>
+                <li><a href="Reporte/HURM-003.html">Facturación</a></li>
+                <li><a href="Reporte/HURM-004-1.html">Lista de eventos</a></li>
+                <li><a href="Reporte/HURM-004-2.html">Reporte de reservas</a></li>
+                <li><a href="Reporte/HURM-005-1.html">Reporte de retroalimentacion</a></li>
+                <li><a href="Reporte/HURM-006-1.html">Planes de dietas</a></li>
+                <li><a href="Reporte/HURM-006-2.html">Log de errores</a></li>
+                <li><a href="Reporte/HURM-007.html">Ver historial</a></li>
+            </ul>
+    
+        </li>
+    
+    </ul>
+    `
+    document.getElementById('navegacionMobile').innerHTML = htmlmenuMobile;
+    });
