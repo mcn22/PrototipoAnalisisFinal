590a2d4a0998d244f799108540be246f0d31d9c1
Mario Sanchez <mcn_09@live.com>
2021-05-31 18:24:12 -0600

    assets

--- public/js/validation.js
+++
@@ -1,7 +0,0 @@
-function validateData(){
-  if ($("#fPassword").val() === $("#sPassword").val()) {
-    return true;
-  }else{
-    alert("Contraseñas no coinciden favor corregir este error.");
-  }
-}
